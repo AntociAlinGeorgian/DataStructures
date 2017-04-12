bed1ca25d002c241c276a55be6c86dd97079a3bf
Alin Georgian Antoci <brinza.alin.georgian@gmail.com>
2017-04-12 15:38:24 +0300

    [fix] - fixed the insert function

--- HashTable/HashTable/Source.cpp
+++ HashTable/HashTable/Source.cpp
@@ -2,8 +2,8 @@
 #include<stdio.h>
 
 struct Student {
-	char* nume;
 	int nota;
+	char* nume;
 };
 
 struct Nod {
@@ -16,17 +16,15 @@ struct HashTable {
 	int dimHT;
 };
 
-//functia hash
 int fhash(int nota, int dim) {
-	return nota%dim;
+	return(nota%dim);
 }
 
-Nod* creareNod(Student* s) {
-	Nod* n = new Nod;
-	n->info = s;
-	n->next = NULL;
-
-	return n;
+Nod* creareNod(Student* stud) {
+	Nod* nod = new Nod;
+	nod->info = stud;
+	nod->next = NULL;
+	return nod;
 }
 
 HashTable* initHT(int dim) {
@@ -37,6 +35,7 @@ HashTable* initHT(int dim) {
 	for (int i = 0; i < ht->dimHT; i++) {
 		ht->vector[i] = NULL;
 	}
+
 	return ht;
 }
 
@@ -48,76 +47,23 @@ void inserareHT(HashTable*& ht, Nod* nod) {
 	}
 	else {
 		while (poz < ht->dimHT && ht->vector[poz] != NULL) {
-			//parcurgem vectorul pornind de la pozitia in care am gasit cheia in jos
 			poz++;
 		}
 		if (poz == ht->dimHT) {
-			//daca am ajuns la finalul vectorului
-			poz = fhash(nod->info->nota, ht->dimHT);
+			int poz = fhash(nod->info->nota, ht->dimHT);
 			while (poz > 0 && ht->vector[poz] != NULL) {
-				//parcurgem vectorul pornind de la pozitia in care am gasit cheia in jos
 				poz--;
-
-				if (poz == 0) {
-					//daca ajungem la inceputul vectorului apare coliziunea de tip linear probing
-					//si nu putem insera elementul pentru ca vectorul este ocupat in totalitate
-					printf("\nColiziune!");
-				}
-				else {
-					ht->vector[poz] = nod;
-				}
 			}
+			if (poz == 0) {
+				printf("coliziune!\n");
+			}
+			else {
+				ht->vector[poz] = nod;
+			}
+	
 		}
 		else {
 			ht->vector[poz] = nod;
-		}
-	}
-}
-
-Nod* cautareHT(HashTable* ht, int nota) {
-	Nod* nodGasit = nullptr;
-	int poz = fhash(nota, ht->dimHT);
-
-	if (ht->vector[poz] != NULL) {
-		if (ht->vector[poz]->info->nota == nota) {
-			nodGasit = ht->vector[poz];
-		}
-	}
-	else {
-		while (poz < ht->dimHT && ht->vector[poz] != NULL) {
-			if (ht->vector[poz]->info->nota == nota) {
-				nodGasit = ht->vector[poz];
-			}
-			else {
-				poz++;
-			}
-		}
-		if (nodGasit == nullptr) {
-			int poz = fhash(nota, ht->dimHT);
-			
-			while (poz > 0 && ht->vector[poz] != NULL) {
-				if (ht->vector[poz]->info->nota == nota) {
-					nodGasit = ht->vector[poz];
-				}
-				else {
-					poz--;
-				}
-			}
-		}
-	}
-	if (nodGasit != nullptr) {
-		printf("Rezultat: %d: %s - %d", poz, nodGasit->info->nume, nodGasit->info->nota);
-		return nodGasit;
-	}
-	else {
-		return nullptr;
-	}
-}
-
-void afisareHT(HashTable* ht) {
-	for (int i = 0; i < ht->dimHT; i++) {
-		if (ht->vector[i] != NULL) {
-			printf("%d. %s - %d\n", i, ht->vector[i]->info->nume, ht->vector[i]->info->nota);
 		}
 	}
 }
@@ -126,22 +72,73 @@ void citireFisier(HashTable*& ht) {
 	FILE* f = fopen("Text.txt", "r");
 	if (f) {
 		while (!feof(f)) {
-			Student* stud = new Student;
+			Student* s = new Student;
 			char aux[100];
-			fscanf(f, "%s", &aux);
-			stud->nume = (char*)malloc(strlen(aux) + 1);
-			strcpy(stud->nume, aux);
-			fscanf(f, "%d", &stud->nota);
 
-			Nod* nod = creareNod(stud);
-			inserareHT(ht, nod);
+			fscanf(f, "%s", &aux);
+			s->nume = (char*)malloc(strlen(aux) + 1);
+			strcpy(s->nume, aux);
+
+			fscanf(f, "%d", &s->nota);
+
+			Nod* n = creareNod(s);
+			inserareHT(ht, n);
 		}
 	}
 }
 
-void stergereHT(HashTable*& ht, int nota) {
-	bool gasit = false;
+void parcurgere(HashTable* ht) {
+	if (ht) {
+		for (int i = 0; i < ht->dimHT; i++) {
+			if (ht->vector[i] != NULL) {
+				printf("%d. %s - %d \n", i, ht->vector[i]->info->nume, ht->vector[i]->info->nota);
+			}
+		}
+	}
+}
+
+
+void cautare(HashTable* ht, int nota) {
 	int poz = fhash(nota, ht->dimHT);
+	bool gasit = false;
+
+	if (ht->vector[poz] != NULL) {
+		if (ht->vector[poz]->info->nota == nota) {
+			printf("%d. %s - %d \n", poz, ht->vector[poz]->info->nume, ht->vector[poz]->info->nota);
+			gasit = true;
+		}
+	}
+	else {
+		while (poz < ht->dimHT && ht->vector[poz] != NULL) {
+			if (ht->vector[poz]->info->nota == nota) {
+				printf("%d. %s - %d \n", poz, ht->vector[poz]->info->nume, ht->vector[poz]->info->nota);
+				gasit = true;
+			}
+			else {
+				poz++;
+			}
+		}
+		if (gasit == false) {
+			int poz = fhash(nota, ht->dimHT);
+			while (poz > 0 && ht->vector[poz] != NULL) {
+				if (ht->vector[poz]->info->nota == nota) {
+					printf("%d. %s - %d \n", poz, ht->vector[poz]->info->nume, ht->vector[poz]->info->nota);
+					gasit = true;
+				}
+				else {
+					poz--;
+				}
+			}
+		}
+	}
+	if (gasit == false) {
+		printf("Nu exista");
+	}
+}
+
+void stergereNod(HashTable*& ht, int nota) {
+	int poz = fhash(nota, ht->dimHT);
+	bool gasit = false;
 
 	if (ht->vector[poz] != NULL) {
 		if (ht->vector[poz]->info->nota == nota) {
@@ -161,12 +158,13 @@ void stergereHT(HashTable*& ht, int nota) {
 				poz++;
 			}
 		}
-		if (gasit == false) {
+		if (poz == ht->dimHT) {
 			int poz = fhash(nota, ht->dimHT);
-			while (poz > ht->dimHT && ht->vector[poz] != NULL) {
+
+			while (poz > 0 && ht->vector[poz] != NULL) {
 				if (ht->vector[poz]->info->nota == nota) {
 					free(ht->vector[poz]);
-					ht->vector[poz] == NULL;
+					ht->vector[poz] = NULL;
 					gasit = true;
 				}
 				else {
@@ -176,22 +174,17 @@ void stergereHT(HashTable*& ht, int nota) {
 		}
 	}
 	if (gasit == false) {
-		printf("nu exista aceasta nota.\n");
+		printf("nu s-a gasit.");
 	}
-	else {
-		printf("Notele %d au fost sterse.\n",nota);
-	}
-
 }
 
 void main() {
-	HashTable* ht = initHT(27);
+	HashTable* ht = initHT(20);
 	citireFisier(ht);
-	afisareHT(ht);
+	parcurgere(ht);
 
-	//cautareHT(ht, 5);
+	cautare(ht, 11);
 
-	stergereHT(ht, 5);
-
-	afisareHT(ht);
+	stergereNod(ht,5);
+	parcurgere(ht);
 }
