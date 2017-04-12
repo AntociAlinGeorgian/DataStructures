ffcfc50711920385690086f6f9b4e2a58acb753d
Alin Georgian Antoci <brinza.alin.georgian@gmail.com>
2017-04-12 12:44:33 +0300

    [new] - Delete a node from ht

--- HashTable/HashTable/Source.cpp
+++ HashTable/HashTable/Source.cpp
@@ -73,3 +73,125 @@ void inserareHT(HashTable*& ht, Nod* nod) {
 		}
 	}
 }
+
+Nod* cautareHT(HashTable* ht, int nota) {
+	Nod* nodGasit = nullptr;
+	int poz = fhash(nota, ht->dimHT);
+
+	if (ht->vector[poz] != NULL) {
+		if (ht->vector[poz]->info->nota == nota) {
+			nodGasit = ht->vector[poz];
+		}
+	}
+	else {
+		while (poz < ht->dimHT && ht->vector[poz] != NULL) {
+			if (ht->vector[poz]->info->nota == nota) {
+				nodGasit = ht->vector[poz];
+			}
+			else {
+				poz++;
+			}
+		}
+		if (nodGasit == nullptr) {
+			int poz = fhash(nota, ht->dimHT);
+			
+			while (poz > 0 && ht->vector[poz] != NULL) {
+				if (ht->vector[poz]->info->nota == nota) {
+					nodGasit = ht->vector[poz];
+				}
+				else {
+					poz--;
+				}
+			}
+		}
+	}
+	if (nodGasit != nullptr) {
+		printf("Rezultat: %d: %s - %d", poz, nodGasit->info->nume, nodGasit->info->nota);
+		return nodGasit;
+	}
+	else {
+		return nullptr;
+	}
+}
+
+void afisareHT(HashTable* ht) {
+	for (int i = 0; i < ht->dimHT; i++) {
+		if (ht->vector[i] != NULL) {
+			printf("%d. %s - %d\n", i, ht->vector[i]->info->nume, ht->vector[i]->info->nota);
+		}
+	}
+}
+
+void citireFisier(HashTable*& ht) {
+	FILE* f = fopen("Text.txt", "r");
+	if (f) {
+		while (!feof(f)) {
+			Student* stud = new Student;
+			char aux[100];
+			fscanf(f, "%s", &aux);
+			stud->nume = (char*)malloc(strlen(aux) + 1);
+			strcpy(stud->nume, aux);
+			fscanf(f, "%d", &stud->nota);
+
+			Nod* nod = creareNod(stud);
+			inserareHT(ht, nod);
+		}
+	}
+}
+
+void stergereHT(HashTable*& ht, int nota) {
+	bool gasit = false;
+	int poz = fhash(nota, ht->dimHT);
+
+	if (ht->vector[poz] != NULL) {
+		if (ht->vector[poz]->info->nota == nota) {
+			free(ht->vector[poz]);
+			ht->vector[poz] = NULL;
+			gasit = true;
+		}
+	}
+	else {
+		while (poz < ht->dimHT && ht->vector[poz] != NULL) {
+			if (ht->vector[poz]->info->nota == nota) {
+				free(ht->vector[poz]);
+				ht->vector[poz] = NULL;
+				gasit = true;
+			}
+			else {
+				poz++;
+			}
+		}
+		if (gasit == false) {
+			int poz = fhash(nota, ht->dimHT);
+			while (poz > ht->dimHT && ht->vector[poz] != NULL) {
+				if (ht->vector[poz]->info->nota == nota) {
+					free(ht->vector[poz]);
+					ht->vector[poz] == NULL;
+					gasit = true;
+				}
+				else {
+					poz--;
+				}
+			}
+		}
+	}
+	if (gasit == false) {
+		printf("nu exista aceasta nota.\n");
+	}
+	else {
+		printf("Notele %d au fost sterse.\n",nota);
+	}
+
+}
+
+void main() {
+	HashTable* ht = initHT(27);
+	citireFisier(ht);
+	afisareHT(ht);
+
+	//cautareHT(ht, 5);
+
+	stergereHT(ht, 5);
+
+	afisareHT(ht);
+}
